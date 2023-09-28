9559c3527ad2d5ac343f0d941330d6f0a63f0fdd
Ilhan Ates <ilhanates@duck.com>
2023-09-28 23:59:59 +0300

    🎨 Cleanup + github icon

--- src/main.ts
+++ src/main.ts
@@ -1,103 +1,85 @@
-import QRCode, { QRCodeMaskPattern, QRCodeToBufferOptions, QRCodeToDataURLOptions, QRCodeToDataURLOptionsJpegWebp } from 'qrcode';
+import QRCode, { QRCodeMaskPattern, QRCodeToBufferOptions } from 'qrcode';
 import { getStaticFile, interpolate } from './utils';
 
-export default async ({ req, res, log, error }: any) => {
-  // Endpoint with form for OG image URL generation
-  if (req.path === "/") {
-    const html = interpolate(getStaticFile("index.html"), {
-      HOSTNAME: req.host,
-    });
-    return res.send(html, 200, { "Content-Type": "text/html; charset=utf-8" });
-  }
-  if (req.path === "/favicon.ico") {
-    const pngBytes = await QRCode.toBuffer("https://qr.ilhan.me");
-    return res.send(pngBytes, 200, {
-      "Content-Type": "image/png",
-    });
-  }
-  if (req.path === "/qr") {
-    // parse options
-    const dataValue = req.query.data;
-    if (!dataValue) {
-      return res.json({
-        error: "Missing data query parameter",
+export default async ({ req, res }: any) => {
+  switch (req.path) {
+    case "/":
+      const html = interpolate(getStaticFile("index.html"), {
+        HOSTNAME: req.host,
       });
-    }
-    const data = decodeURIComponent(dataValue);
-
-    // parse the type first
-    let options: QRCodeToBufferOptions = {};
-
-    const version = req.query.version;
-    if (version) {
-      options.version = parseInt(version);
-    }
-
-    const errorCorrectionLevel = req.query.errorCorrectionLevel;
-    if (errorCorrectionLevel) {
-      if (!["L", "M", "Q", "H"].includes(errorCorrectionLevel)) {
+      return res.send(html, 200, { "Content-Type": "text/html; charset=utf-8" });
+    
+    case "/favicon.ico":
+      return res.send(await QRCode.toBuffer("https://qr.ilhan.me"), 200, {
+        "Content-Type": "image/png",
+      });
+    
+    case "/qr":
+      
+      const dataValue = req.query.data;
+      if (!dataValue) {
         return res.json({
-          error: "errorCorrectionLevel must be one of L, M, Q, H",
+          error: "Missing data query parameter",
         });
       }
-      options.errorCorrectionLevel = errorCorrectionLevel;
-    }
+      const data = decodeURIComponent(dataValue);
 
-    const maskPattern = req.query.maskPattern;
-    if (maskPattern) {
-      let maskPatternInt = parseInt(maskPattern);
-      if (maskPatternInt < 0 || maskPatternInt > 7) {
-        return res.json({
-          error: "maskPattern must be between 0 and 7",
-        });
+      // parse options
+      let options: QRCodeToBufferOptions = {};
+      const version = req.query.version;
+      if (version) {
+        options.version = parseInt(version);
       }
-      options.maskPattern = maskPatternInt as QRCodeMaskPattern;
-    }
-    const margin = req.query.margin;
-    if (margin) {
-      options.margin = parseInt(margin);
-    }
-    const scale = req.query.scale;
-    if (scale) {
-      options.scale = parseInt(scale);
-    }
-    const width = req.query.width;
-    if (width) {
-      options.width = parseInt(width);
-    }
-    const colorLight = req.query.colorLight;
-    if (colorLight) {
-      options.color = {
-        light: colorLight,
-      };
-    }
-    const colorDark = req.query.colorDark;
-    if (colorDark) {
-      if (!options.color) {
-        options.color = {};
+      const errorCorrectionLevel = req.query.errorCorrectionLevel;
+      if (errorCorrectionLevel && ["L", "M", "Q", "H"].includes(errorCorrectionLevel)) {
+        options.errorCorrectionLevel = errorCorrectionLevel;
       }
-      options.color.dark = colorDark;
-    }
+      const maskPattern = req.query.maskPattern;
+      if (maskPattern && parseInt(maskPattern) >= 0 && parseInt(maskPattern) <= 7) {
+        options.maskPattern = parseInt(maskPattern) as QRCodeMaskPattern;
+      }
+      const margin = req.query.margin;
+      if (margin) {
+        options.margin = parseInt(margin);
+      }
+      const scale = req.query.scale;
+      if (scale) {
+        options.scale = parseInt(scale);
+      }
+      const width = req.query.width;
+      if (width) {
+        options.width = parseInt(width);
+      }
+      const colorLight = req.query.colorLight;
+      if (colorLight) {
+        options.color = {
+          light: colorLight,
+        };
+      }
+      const colorDark = req.query.colorDark;
+      if (colorDark) {
+        if (!options.color) {
+          options.color = {};
+        }
+        options.color.dark = colorDark;
+      }
+      const deflateLevel = req.query.deflateLevel;
+      if (deflateLevel) {
+        if (!options.rendererOpts) {
+          options.rendererOpts = {};
+        }
+        options.rendererOpts.deflateLevel = parseInt(deflateLevel);
+      }
+      const deflateStrategy = req.query.deflateStrategy;
+      if (deflateStrategy) {
+        if (!options.rendererOpts) {
+          options.rendererOpts = {};
+        }
+        options.rendererOpts.deflateStrategy = parseInt(deflateStrategy);
+      }
 
-    const deflateLevel = req.query.deflateLevel;
-    if (deflateLevel) {
-      if (!options.rendererOpts) {
-        options.rendererOpts = {};
-      }
-      options.rendererOpts.deflateLevel = parseInt(deflateLevel);
-    }
-    const deflateStrategy = req.query.deflateStrategy;
-    if (deflateStrategy) {
-      if (!options.rendererOpts) {
-        options.rendererOpts = {};
-      }
-      options.rendererOpts.deflateStrategy = parseInt(deflateStrategy);
-    }
-
-    // return as png bytes
-    const pngBytes = await QRCode.toBuffer(data, options);
-    return res.send(pngBytes, 200, {
-      "Content-Type": "image/png",
-    });
+      return res.send(await QRCode.toBuffer(data, options), 200, {
+        "Content-Type": "image/png",
+      });
   }
 };
